446ef8822b3a9cac9af571f7ff6eeb349687142f
maliaoreta <maliaoreta@gmail.com>
2016-04-13 11:07:30 -1000

    renames articles and products files

---
+++ db/Products.js
@@ -0,0 +1,52 @@
+var Products = (function () {
+
+  var productArr = [];
+
+  function add (reqBody, res) {
+
+    productArr.push(reqBody);
+    var currIndex = productArr.indexOf(reqBody);
+    productArr[currIndex].id = currIndex;
+    res.json({success: true});
+    // Need to add success:false if product is already in array
+  }
+
+  function edit (reqBody, res) {
+
+    if (reqBody.hasOwnProperty('name')) {
+
+      productArr[reqBody.id].name = reqBody.name;
+    }
+    if (reqBody.hasOwnProperty('price')) {
+
+      productArr[reqBody.id].price = reqBody.price;
+    }
+    if (reqBody.hasOwnProperty('inventory')) {
+
+      productArr[reqBody.id].inventory = reqBody.inventory;
+    }
+
+    return res.send(productArr[reqBody.id]);
+    // OPTIMIZE and validate id
+  }
+
+  function remove (req, res) {
+
+    productArr.splice(req.params.id, 1);
+    console.log(productArr);
+    res.json({success: true});
+
+    // validate success false if product was not in array
+  }
+  
+  return {
+
+    add: add,
+    edit: edit,
+    delete: remove
+  };
+
+})();
+
+
+module.exports = Products;
