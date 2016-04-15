a4015b724b57bc1541473ffd8de6042f1a2a5881
Pamela Yang <ypyang237@gmail.com>
2016-04-15 09:55:21 -1000

    finishes trackerlog middleware

--- middleware/logs/analytics.js
+++ middleware/logs/analytics.js
@@ -1,27 +1,36 @@
 var fs = require('fs');
+var path = require('path');
 
 function analyticTracker () {
 
   return function(req, res, next) {
     var reqMethod = req.originalMethod;
+    var reqUri = req.originalUrl;
+    var reqHeaders = JSON.stringify(req.headers);
+    var d = new Date();
+    var aLog = reqMethod + ' ' + reqUri + ' ' +  d + ' ' + reqHeaders + '\r\n\r\n';
 
-    var options = {
-        flags: 'r+',
-        defaultEncoding: 'utf8',
-        fd: null,
-        mode: 0o666,
-        autoClose: true
-      };
+    // var options = {
+    //     flags: 'r+',
+    //     defaultEncoding: 'utf8',
+    //     fd: null,
+    //     mode: 0o666,
+    //     autoClose: true
+    //   };
     // fs.readdir('../middleware/logs', function(err, files) {
     //   if(err) {
     //     console.log('error');
     //   }
-      var analytics = fs.createWriteStream('../articles-products-and-express/middleware/logs/analyticTracker.log', options);
-      analytics.write(reqMethod);
+      var filePath = path.resolve(process.cwd(), 'middleware/logs/analyticTracker.log');
+      var analytics = fs.appendFile(filePath, aLog, 'utf8', function(err) {
+        if(err) {
+          throw new Error(err);
+        }
+      });
+      // analytics.end(aLog);
 
-        //Need to fix path to relative, and utilze fs.readdir for timestamps
-    // });
 
+    // console.log(aLog);
 
     return next();
   };
--- routes/products.js
+++ routes/products.js
@@ -11,7 +11,7 @@ var express = require('express'),
 var pCollection = productModel.get();
 
 router.route('/')
-  .post(analyticTracker(), function (req, res) {
+  .post(analyticTracker(), analyticTracker(), function (req, res) {
 
     var postObj = {
 
@@ -23,13 +23,13 @@ router.route('/')
     productModel.add(postObj, res);
     return res.json({success: true});
   })
-  .get(function(req, res) {
+  .get(analyticTracker(), function(req, res) {
 
       res.render('products/index', {products: pCollection});
   });
 
 router.route('/:id')
-  .put(function (req, res) {
+  .put(analyticTracker(), function (req, res) {
 
     var editObj = {
 
@@ -37,12 +37,12 @@ router.route('/:id')
       price: req.body.price,
       inventory: req.body.inventory,
       id: req.body.id
-    }
+    };
 
     productModel.edit(editObj, res);
-    return res.json({success: true})
+    return res.json({success: true});
   })
-  .delete(function (req, res) {
+  .delete(analyticTracker(), function (req, res) {
 
     var reqID = req.params.id;
 
@@ -55,9 +55,9 @@ router.route('/:id/edit').get(function(req, res) {
   res.render('products/edit', {product: pCollection[req.params.id]});
 });
 
-router.route('/new').get(function (req, res) {
+router.route('/new').get(analyticTracker(), function (req, res) {
 
   res.render('products/new');
-})
+});
 
 module.exports = router;
