6227ce86aa69e7813fc5e2ef5780bd1a562d9897
maliaoreta <maliaoreta@gmail.com>
2016-04-15 16:58:25 -1000

    removes comments

--- middleware/logs/analytics.js
+++ middleware/logs/analytics.js
@@ -10,27 +10,12 @@ function analyticTracker () {
     var d = new Date();
     var aLog = reqMethod + ' ' + reqUri + ' ' +  d + ' ' + reqHeaders + '\r\n\r\n';
 
-    // var options = {
-    //     flags: 'r+',
-    //     defaultEncoding: 'utf8',
-    //     fd: null,
-    //     mode: 0o666,
-    //     autoClose: true
-    //   };
-    // fs.readdir('../middleware/logs', function(err, files) {
-    //   if(err) {
-    //     console.log('error');
-    //   }
       var filePath = path.resolve(process.cwd(), 'middleware/logs/analyticTracker.log');
       var analytics = fs.appendFile(filePath, aLog, 'utf8', function(err) {
         if(err) {
           throw new Error(err);
         }
       });
-      // analytics.end(aLog);
-
-
-    // console.log(aLog);
 
     return next();
   };
