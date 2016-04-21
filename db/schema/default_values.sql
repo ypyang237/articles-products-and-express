a86fbb5022407da9472611d12bf23c5e74694d5a
maliaoreta <maliaoreta@gmail.com>
2016-04-21 12:03:03 -1000

    fixes urlTitle

--- db/schema/default_values.sql
+++ db/schema/default_values.sql
@@ -5,9 +5,9 @@ INSERT INTO products (name, price, inventory) VALUES ('brownie', '6', '11');
 INSERT INTO products (name, price, inventory) VALUES ('cake', '7', '12');
 
 
-INSERT INTO articles (title, body, author) VALUES ('Save The World', 'SuperMan is here', 'Pam');
-INSERT INTO articles (title, body, author) VALUES ('The Best Cookies', 'I like white chocolate chip macademia nut cookies', 'Malia');
-INSERT INTO articles (title, body, author) VALUES ('Out to hauntya', 'Do not sleep', 'BoogyMan');
+INSERT INTO articles (title, body, author, urlTitle) VALUES ('Save The World', 'SuperMan is here', 'Pam', 'Save%20The%20World');
+INSERT INTO articles (title, body, author, urlTitle) VALUES ('The Best Cookies', 'I like white chocolate chip macademia nut cookies', 'Malia', 'The%20Best%20Cookies');
+INSERT INTO articles (title, body, author, urlTitle) VALUES ('Out to hauntya', 'Do not sleep', 'BoogyMan', 'Out%20to%20hauntya');
 
 
 -- SELECT * FROM products;
