97bdf60c6ce094726618f07b48282be8cbffedec
maliaoreta <maliaoreta@gmail.com>
2016-04-21 14:55:04 -1000

    fixes validation for data types

--- middleware/dataTypeValidation.js
+++ middleware/dataTypeValidation.js
@@ -2,10 +2,20 @@ function dataTypeValidation (validValues) {
 // merge with form validation, use obj.keys
   return function (req, res, next) {
 
-    var temp = Object.keys(req.body).every(function (key) {
+    //if temp is true its ok
+    var check = Object.keys(req.body).every(function (key) {
 
-      return typeof req.body[key] === parseInt(validValues[key]).toString;
-    })
+      if (validValues[key] === 'number') {
+        return parseInt(req.body[key]).toString() == req.body[key]; // typeof req.body.id = 'number', validValues['id'] = 'number'
+      }
+      else {
+        return typeof req.body[key] === validValues[key];
+      }
+    });
+
+    if (check === false) {
+      return res.status(400).send('Incorrect data type');
+    }
 
     return next();
   };
