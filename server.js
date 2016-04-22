bce982c1f3f8e9b61363973f1fef93aa301e2cb0
Pamela Yang <ypyang237@gmail.com>
2016-04-21 19:22:10 -1000

    double checks express.use(static) is there

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(express.static('public'));
 
 app.use(methodOverride(function(req, res){
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
     var method = req.body._method;
     delete req.body._method;
     return method;
